099a2da84ac4881f110157fa1c036e1099bbb9d6
Kamil Przebieracz <dmjs23@gmail.com>
2022-03-16 18:16:14 +0100

    zad1 - done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryBox = document.querySelector("div.gallery");
+
+galleryItems.forEach((el) => {
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = el.original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = el.preview;
+  galleryImage.alt = el.description;
+
+  galleryBox.append(galleryLink);
+  galleryLink.append(galleryImage);
+});
+
+galleryBox.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  console.log(ev.target.alt);
+});
+
+galleryBox.onclick = (ev) => {
+  const lightbox = new SimpleLightbox(".gallery a", {
+    spinner: false,
+    captionsData: "alt",
+    captionPosition: "outside",
+    captionDelay: 250,
+  });
+};
