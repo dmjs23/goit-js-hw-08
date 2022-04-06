2359dc94fee6166152c6135f37479f61ef21d791
Kamil Przebieracz <dmjs23@gmail.com>
2022-04-06 14:50:43 +0200

    zad.2-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+//DOM import
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe, {
+    url: 'https://player.vimeo.com/video/236203659',
+    
+});
 
+//Export to localstorage
+const timeUpdater = data => {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
+};
+//Import from localstorage
+const lastTime = localStorage.getItem('videoplayer-current-time');
+const parsedLastTime = JSON.parse(lastTime);
+//Start player whit lisiner
+player.on('timeupdate', throttle(timeUpdater, 1000));
+//API 
+player
+  .setCurrentTime(parsedLastTime)
+  .then(function (seconds) {
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import _ from "lodash";
 
+const $form = document.querySelector("form.feedback-form");
+
+const feedbackFromState = {
+    email: null,
+    message: null,
+};
+
+$form.elements.email.addEventListener("input", _.throttle((ev) => {
+    feedbackFromState.email = ev.target.value;
+    localStorage.setItem('feedback-from-state', JSON.stringify(feedbackFromState));
+}, 500));
+
+$form.elements.message.addEventListener("input", _.throttle((ev) => {
+        feedbackFromState.message = ev.target.value;
+        localStorage.setItem("feedback-from-state", JSON.stringify(feedbackFromState));
+}, 500));
+
+$form.lastElementChild.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    localStorage.clear();
+    $form.reset();
+    console.log(feedbackFromState);
+});
+
+let parsedForm = JSON.parse(localStorage.getItem("feedback-from-state"));
+
+if (parsedForm !== null) {
+    $form.elements.email.value = parsedForm.email;
+    $form.elements.message.value = parsedForm.message;
+}
